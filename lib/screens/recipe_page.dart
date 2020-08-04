67d610c2ca5a36b53c5c066a8f3fe5495834058d
derkyeng <dyang@hamilton.edu>
2020-08-04 18:37:24 -0500

    final ui change

--- lib/screens/recipe_page.dart
+++ lib/screens/recipe_page.dart
@@ -18,21 +18,24 @@ class _RecipePageState extends State<RecipePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        centerTitle: true,
-        title: Text(
-          'Recipe Finder',
-          style: TextStyle(
-            color: Colors.purple[200],
+      appBar: PreferredSize(
+        preferredSize: Size.fromHeight(50.0),
+        child: AppBar(
+          centerTitle: true,
+          title: Text(
+            'Recipe Finder',
+            style: TextStyle(
+              color: Colors.purple[200],
+            ),
           ),
+          backgroundColor: Colors.transparent,
+          elevation: 0.0,
         ),
-        backgroundColor: Colors.transparent,
-        elevation: 0.0,
       ),
       body: Column(
         children: <Widget>[
           Container(
-            padding: EdgeInsets.all(16.0),
+            padding: EdgeInsets.fromLTRB(15.0, 0, 15.0, 15.0),
             decoration: BoxDecoration(
               borderRadius: BorderRadius.circular(10.0),
             ),
@@ -69,50 +72,37 @@ class _RecipePageState extends State<RecipePage> {
                       print(recipeMaker[index].url);
                       _launchURL(recipeMaker[index].url);
                     },
-                    child: Container(
-                      alignment: Alignment.bottomRight,
-                      child: FittedBox(
-                        child: Container(
-                          color: Color.fromRGBO(204, 223, 252, .3),
+                    child: Stack(
+                      fit: StackFit.loose,
+                      children: <Widget>[
+                        Container(
+                          height: 165.0,
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(10),
+                            image: DecorationImage(
+                              image: NetworkImage(
+                                  'https://spoonacular.com/recipeImages/' +
+                                      recipeMaker[index].id.toString() +
+                                      '-636x393.jpg'),
+                              fit: BoxFit.fill,
+                            ),
+                          ),
+                        ),
+                        Positioned(
+                          bottom: 0.0,
                           child: RichText(
                             overflow: TextOverflow.ellipsis,
                             text: TextSpan(
                               text: limitText(recipeMaker[index].title),
                               style: TextStyle(
-                                color: Colors.white,
-                                shadows: [
-                                  Shadow(
-                                    offset: Offset(-1.0, -1.0),
-                                    color: Colors.black,
-                                  ),
-                                  Shadow(
-                                    offset: Offset(1.0, -1.0),
-                                    color: Colors.black,
-                                  ),
-                                  Shadow(
-                                    offset: Offset(1.0, 1.0),
-                                    color: Colors.black,
-                                  ),
-                                  Shadow(
-                                    offset: Offset(-1.0, 1.0),
-                                    color: Colors.black,
-                                  ),
-                                ],
+                                color: Colors.black,
+                                fontWeight: FontWeight.w600,
+                                fontFamily: 'OpenSans',
                               ),
                             ),
                           ),
                         ),
-                      ),
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(10),
-                        image: DecorationImage(
-                          image: NetworkImage(
-                              'https://spoonacular.com/recipeImages/' +
-                                  recipeMaker[index].id.toString() +
-                                  '-636x393.jpg'),
-                          fit: BoxFit.fill,
-                        ),
-                      ),
+                      ],
                     ),
                   ),
                 );
@@ -143,8 +133,8 @@ class _RecipePageState extends State<RecipePage> {
   }
 
   String limitText(String text) {
-    if (text.length > 15) {
-      return text.substring(0, 15) + '...';
+    if (text.length > 20) {
+      return text.substring(0, 1).toUpperCase() + text.substring(1, 20) + '...';
     }
     return text;
   }
